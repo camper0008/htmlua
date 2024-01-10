73dd76d4c1b7721aa817cf97ee1769ea1d9900b1
Theis Pieter Hollebeek <tphollebeek@gmail.com>
2024-01-11 00:27:34 +0100

    cgi example

---
+++ cgi/run.sh
@@ -0,0 +1,1 @@
+httpd -v -f -p 8080 -h ./www -c httpd.conf
---
+++ cgi/www/cgi-bin/index.lua
@@ -0,0 +1,31 @@
+local htmlua = require "htmlua"
+require "htmlua.elements"
+
+local doc = html {
+    lang = "en",
+    head {
+        title "My document",
+        style [[
+            html {
+                color-scheme: light dark;
+            }
+            p {
+                font-size: 16px;
+            }  
+        ]],
+    },
+    body {
+        h1 "My document",
+        p "Here comes some cool text or something",
+        p {
+            class = 'quote"test',
+            "Haven't really decided yet <p>",
+            " ",
+            os.date "Today's date is %d/%m-%Y",
+        },
+    },
+}
+
+print("content-type: text/html")
+print()
+print(htmlua.render(doc))
